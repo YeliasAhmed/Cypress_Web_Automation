82245a58614f87d129b9c269aba99312dd02f224
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-07-17 02:36:32 +0600

    cypress web automation

---
+++ cypress/Locators/locators.js
@@ -0,0 +1,43 @@
+class locatorsPage {
+
+  // DemoBlaze Homepage
+  productStore = "//a[@id='nava']"
+  home = "(//a[@class='nav-link'])[1]"
+  contact = "//a[normalize-space()='Contact']"
+  aboutUs = "//a[normalize-space()='About us']"
+  cart = "//a[@id='cartur']"
+  login = "//a[@id='login2']"
+  signUp = "//a[@id='signin2']"
+  catrgory = "//a[@id='cat']"
+  catrgoryPhone = "(//a[normalize-space()='Phones'])[1]"
+  catrgoryLaptop = "(//a[normalize-space()='Laptops'])[1]"
+  catrgoryMonitor = "(//a[normalize-space()='Monitors'])[1]"
+  previousButton = "//button[@id='prev2']"
+  nextButton = "//button[@id='next2']"
+  footer = "//div[@id='footc']"
+  firstPageItem1 = "(//div[@class='card h-100'])[1]"
+  firstPageItem6 = "(//div[@class='card h-100'])[6]"
+  firstPageItem9 = "(//div[@class='card h-100'])[9]"
+  nextPageItem2 = "(//div[@class='card h-100'])[2]"
+  nextPageItem5 = "(//div[@class='card h-100'])[5]"
+
+  mobileGalaxy = "//a[normalize-space()='Samsung galaxy s6']"
+  mobileNexus = "//a[normalize-space()='Nexus 6']"
+  mobileIPhone = "//a[normalize-space()='Iphone 6 32gb']"
+
+  laptopSony = "//a[normalize-space()='Sony vaio i7']"
+  laptopMac = "//a[normalize-space()='MacBook air']"
+
+  montorApple = "//a[normalize-space()='Apple monitor 24']"
+  montorAsus = "//a[normalize-space()='ASUS Full HD']"
+
+  signupUsername = "//input[@id='sign-username']"
+  signupPassword = "//input[@id='sign-password']"
+  signupButton = "//button[normalize-space()='Sign up']"
+
+
+
+// -------------------------------------Demoblaze End----------------------------------------
+
+}
+export default locatorsPage;
